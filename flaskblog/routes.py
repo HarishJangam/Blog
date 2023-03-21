ae5b14c239abd40867f19b23a652aa1042dff067
harish <jangamharish949.com>
2023-03-21 14:34:04 +0530

    adding validations

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -10,12 +10,66 @@ users = []
 DBNAME = "users.json"
 active_logins = []
 
+
+def excess_data(user):
+    for keys in user.keys():
+        if keys=="fullname":
+            continue
+        elif keys=="age":
+            continue
+        elif keys=="username":
+            continue
+        elif keys=="password":
+            continue
+        else:
+            return True,"excess data not allowed"
+    return False,"valid"
+
+
+def validate_registretion(user):
+    if "fullname" not in user:
+        return False,"fullname is mandatory"
+    if "age" not in user:
+        return False,"age is mandatory"
+    if "username" not in user:
+        return False,"username is mandatory"
+    if "password" not in user:
+        return False,"password is mandatory"
+    if len(user)!=4:
+        return False,"check again"
+    return True,"valid"
+
+def validate_register_values(user):
+    
+    username=user["username"]
+    age=user["age"]
+    fullname=user["fullname"]
+    password=user["password"]
+    if type(username)!=str or len(username)<5 or username.find(" ")!=-1:
+        return True,"username should be string and length greater than 5 charectors"
+    if type(age)!=int or age==0:
+        return True,"age should be integer and greater than 0"
+    if type(password)!=str or len(password)<7 or password.find(" ")!=-1:
+        return True,"password should be string and greater than 7 charectors and not contain spaces"
+    return False,""
+
+
+def validate_login(user):
+    if "username" not in user:
+        return False,"username is mandatory"
+    if "password" not in user:
+        return False,"password is mandatory"
+    return True,"valid"
+
+
 def save_users(users) -> None:
     with open("users.json", "w") as f:
         json.dump(users, f)
 
 def user_already_exists(users, username) -> bool:
-    pass
+    for user in users:
+        if user.get("username")==username.get("username"):
+            return True
 
 def not_authenticated(token):
     if token in active_logins:
@@ -75,17 +129,33 @@ def register_new_user():
         4. username should be unique
     """
     new_user = request.json # dict
-    user_id = len(users) + 1
-    new_user["user_id"] = user_id
-    users.append(new_user)
-    save_users(users)
-    return new_user
+    print(len(new_user))
+    flag_excess,excess=excess_data(new_user)
+    if flag_excess:
+        return Response(response=json.dumps({"msg":excess}).encode(),content_type="application/json",status=401)
+
+    val,err=validate_registretion(new_user)
+    if val:
+        if user_already_exists(users,new_user):
+            return Response(response=json.dumps({"msg":"username already exist"}).encode(),content_type="application/json",status=200)
+        
+        flag_value,err_values=validate_register_values(new_user)
+        if flag_value:
+            return Response(response=json.dumps({"msg":err_values}).encode(),content_type="application/json",status=401)
+
+        user_id = len(users) + 1
+        new_user["user_id"] = user_id
+        users.append(new_user)
+        save_users(users)
+        return new_user
+    else:
+        return Response(response=json.dumps({"msg":err}).encode(),content_type="application/json",status=401)
 
 @app.route("/user", methods=['GET'])
 def get_all_user():
-    headers = request.headers
-    if not_authenticated(headers.get("token")):
-        return Response(response=json.dumps({"msg": "Please do login"}).encode(),content_type="application/json", status=401)
+    # headers = request.headers
+    # if not_authenticated(headers.get("token")):
+        # return Response(response=json.dumps({"msg": "Please do login"}).encode(),content_type="application/json", status=401)
     return users
 
 @app.route("/user/<username>", methods = ["GET"])
@@ -132,12 +202,20 @@ def do_login():
     """
     creds = request.json
     
-    if invalid_user(users, creds):
-        return Response(response=json.dumps({"msg": "Invalid user"}).encode(),content_type="application/json", status=401)
-    
-    token = str(uuid4())
-    active_logins.append(token)
-    return Response(response=json.dumps({"msg": "Login Successfully"}).encode(),content_type="application/json",status=200, headers={"token": token})
+    flag,excess=excess_data(creds)
+    if flag:
+        return Response(response=json.dumps({"msg":excess}).encode(),content_type="application/json",status=401)
+
+    val,err=validate_login(creds)
+    if val:
+        if invalid_user(users, creds):
+            return Response(response=json.dumps({"msg": "Invalid user"}).encode(),content_type="application/json", status=401)
+        
+        token = str(uuid4())
+        active_logins.append(token)
+        return Response(response=json.dumps({"msg": "Login Successfully"}).encode(),content_type="application/json",status=200, headers={"token": token})
+    else:
+        return Response(response=json.dumps({"msg": err}).encode(),content_type="application/json",status=401,)
 
 
 
